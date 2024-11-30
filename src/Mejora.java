060036c43ee704fa91dd4d31a168b11d26762e85
Pablo2085 <pablosaavedrax2@gmail.com>
2024-11-30 19:02:48 +0100

    Se ha añadido los constructores, getters y setters y métodos a la clase mejora.

---
+++ src/Mejora.java
@@ -0,0 +1,43 @@
+import java.io.Serializable;
+
+public class Mejora implements Serializable{
+    private String nombre;
+    private double porcentajeIncremento;
+
+    //Constructores
+    public Mejora(String nombre, double porcentajeIncremento) {
+        this.nombre = nombre;
+        this.porcentajeIncremento = porcentajeIncremento;
+    }
+
+    //Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPorcentajeIncremento() {
+        return porcentajeIncremento;
+    }
+
+    public void setPorcentajeIncremento(double porcentajeIncremento) {
+        this.porcentajeIncremento = porcentajeIncremento;
+    }
+
+    // Metodo para aplicar el incremento al precio base
+    public double aplicarIncremento(double precioBase) {
+        return precioBase + (precioBase * (porcentajeIncremento / 100));
+    }
+
+    // Metodo para mostrar los detalles de la mejora
+    @Override
+    public String toString() {
+        return "Mejora{" +
+                "nombre='" + nombre + '\'' +
+                ", porcentajeIncremento=" + porcentajeIncremento +
+                "%}";
+    }
+}
