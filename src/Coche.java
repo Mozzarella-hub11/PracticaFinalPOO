7ae11d55397a240cfb80a309e39d0b852527d7d7
=Maxssj42 <=max.salvador@alumno.ucjc.edu>
2024-11-30 19:13:34 +0100

    Añadidos Getters y Setters a Coche Añadidos metodos a Seccion.java

--- src/Coche.java
+++ src/Coche.java
@@ -5,5 +5,27 @@ public class Coche {
     private int stock;
     private List<Mejora>;
 
+    public double getPrecioBase() {
+        return precioBase;
+    }
 
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
--- src/Seccion.java
+++ src/Seccion.java
@@ -16,20 +16,50 @@ public class Seccion {
     public void agregarCoche(Coche coche) {
         if (!this.coches.contains(coche)) {
             this.coches.add(coche);
+
         }else{
             System.out.println("El coche ya esta registrado");
         }
 
     }
 
-    public void eliminarCoche(Coche coche) {
-
-
+    public boolean eliminarCoche(String idCoche) {
+        for (Coche coche : coches) {
+            if (coche.getId().equals(idCoche)) {
+                coches.remove(coche);
+                System.out.println("Coche con ID " + idCoche + " eliminado de la sección.");
+                return true;
+            }
+        }
+        System.out.println("Error: No se encontró el coche con ID " + idCoche + " en la sección.");
+        return false;
     }
 
+
+    public void mostrarSeccion(){
+        System.out.println("Sección ID: " + getId() + ", Descripción: " + getDescripcion());
+    }
+
+    public boolean existeCoche(String idCoche) {
+        for (Coche coche : coches) {
+            if (coche.getId().equals(idCoche)) {
+                return true;  // El coche existe
+            }
+        }
+        return false;  // El coche no existe
+    }
+
+
+
     public void mostrarCoches() {
-
-
+        if (coches.isEmpty()) {
+            System.out.println("No hay coches disponibles en esta sección.");
+        } else {
+            System.out.println("Coches en la sección '" + id + "':");
+            for (Coche coche : coches) {
+                System.out.println(coche);  // Se supone que la clase Coche tiene un método toString()
+            }
+        }
     }
 
     public String getId() {
